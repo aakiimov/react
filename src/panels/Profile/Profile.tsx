7d3e9d8d3034b58ca09a4f0f1773abec85ab3b0b
aakiimov <dragantsevv@ya.ru>
2021-02-03 10:09:03 +0400

    init

--- src/panels/Profile/Profile.tsx
+++ src/panels/Profile/Profile.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Panel, PanelHeader } from '@vkontakte/vkui';
-import { Icon28UserCircleOutline } from '@vkontakte/icons';
+import { Panel, PanelHeader, SimpleCell, Avatar } from '@vkontakte/vkui';
+import { Icon12Verified } from '@vkontakte/icons';
 import { ContentFix } from '../../components';
 import { DashboardCard } from '../../components/Dashboard/modules'
 import IPanelProps from "../../types/panelProps";
-import firebase from "../../firebase";
-import { getCurrentUserId } from "../../utils";
 
 /**
  * The profile panel.
@@ -16,12 +14,11 @@ import { getCurrentUserId } from "../../utils";
 export default function ProfilePanel(props: IPanelProps): React.ReactElement {
   return (
     <Panel id={props.id}>
-      <PanelHeader left={<Icon28UserCircleOutline />} fixed={false}>Профиль</PanelHeader>
+      <PanelHeader fixed={false}>Профиль</PanelHeader>
       <ContentFix>
-        <DashboardCard 
-          title={`${firebase.database().ref(`${getCurrentUserId()}`).once('value')}`}
-          subtitle="Ранг: основатель"
-        />
+        <DashboardCard>
+          <SimpleCell before={<Avatar size={48} src="https://sun9-45.userapi.com/impg/VyVsYMlTp6zduYxe7YwnmumOUG44_cqRN9lFWg/nhRWEaD2wbA.jpg?size=700x690&quality=96&proxy=1&sign=beff4ad658fdba557919b051f49f18e4&type=album" />} badge={<Icon12Verified />} description="Информация о вас">Вадим Акимов</SimpleCell>
+        </DashboardCard>
       </ContentFix>
     </Panel>
   );
