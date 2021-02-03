051806771373f5f3838e07e0212f859466da75fc
aakiimov <dragantsevv@ya.ru>
2021-02-03 10:33:43 +0400

    init

--- src/panels/Profile/Profile.tsx
+++ src/panels/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Panel, PanelHeader, SimpleCell, Avatar } from '@vkontakte/vkui';
-import { Icon12Verified } from '@vkontakte/icons';
+import { Icon28CoinsOutline } from '@vkontakte/icons';
 import { ContentFix } from '../../components';
 import { DashboardCard } from '../../components/Dashboard/modules'
 import IPanelProps from "../../types/panelProps";
@@ -18,6 +18,7 @@ export default function ProfilePanel(props: IPanelProps): React.ReactElement {
       <ContentFix>
         <DashboardCard>
           <SimpleCell before={<Avatar size={48} src="https://sun9-45.userapi.com/impg/VyVsYMlTp6zduYxe7YwnmumOUG44_cqRN9lFWg/nhRWEaD2wbA.jpg?size=700x690&quality=96&proxy=1&sign=beff4ad658fdba557919b051f49f18e4&type=album" />} description="Информация о вас">Вадим Акимов</SimpleCell>
+          <SimpleCell before={<Icon28CoinsOutline width={20} height={20} fill="#FFA500" />}>Баланс: 0</SimpleCell>
         </DashboardCard>
       </ContentFix>
     </Panel>
