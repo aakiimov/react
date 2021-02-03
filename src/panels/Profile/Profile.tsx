a144c92f98d1e6208b6c7820ce77a360eb42558f
aakiimov <dragantsevv@ya.ru>
2021-02-03 10:13:41 +0400

    init

--- src/panels/Profile/Profile.tsx
+++ src/panels/Profile/Profile.tsx
@@ -17,7 +17,7 @@ export default function ProfilePanel(props: IPanelProps): React.ReactElement {
       <PanelHeader fixed={false}>Профиль</PanelHeader>
       <ContentFix>
         <DashboardCard>
-          <SimpleCell before={<Avatar size={48} src="https://sun9-45.userapi.com/impg/VyVsYMlTp6zduYxe7YwnmumOUG44_cqRN9lFWg/nhRWEaD2wbA.jpg?size=700x690&quality=96&proxy=1&sign=beff4ad658fdba557919b051f49f18e4&type=album" />} badge={<Icon12Verified />} description="Информация о вас">Вадим Акимов</SimpleCell>
+          <SimpleCell before={<Avatar size={48} src="https://sun9-45.userapi.com/impg/VyVsYMlTp6zduYxe7YwnmumOUG44_cqRN9lFWg/nhRWEaD2wbA.jpg?size=700x690&quality=96&proxy=1&sign=beff4ad658fdba557919b051f49f18e4&type=album" />} description="Информация о вас">Вадим Акимов</SimpleCell>
         </DashboardCard>
       </ContentFix>
     </Panel>
